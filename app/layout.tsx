b623ce861a2277402b8f0a1677e1c0b6c4d9215c
JXhead <2574303451@qq.com>
2025-08-17 17:37:56 +0800

    three commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: '弹弹堂-今夕公会官网',
   description: 'Created with v0',
-  generator: 'v0.app',
+  generator: '弹弹堂-今夕公会官网',
 }
 
 export default function RootLayout({
